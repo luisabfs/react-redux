38806ce39ec7d65766fb921b27f2280a4ebcdf1c
Luísa Barros <qlullyq@gmail.com>
2021-01-21 17:04:22 -0300

    feat: create root reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,7 @@
 import {createStore} from 'redux';
 
-const store = createStore(() => {
-  return {
-    id: '360f0e61-53bc-48ee-a0a5-82ee7a745b97',
-    name: 'Jane Doe',
-    email: 'jane@doe.com'
-  }
-});
+import rootReducer from './modules/rootReducer';
+
+const store = createStore(rootReducer);
 
 export default store;
