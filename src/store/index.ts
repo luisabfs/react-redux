bc25088f002cd0ff62b18f1d4383e44a0f8499f0
Luísa Barros <qlullyq@gmail.com>
2021-01-21 10:18:24 -0300

    feat: configure redux store

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import {createStore} from 'redux';
+
+const store = createStore(() => {
+  return {
+    id: '360f0e61-53bc-48ee-a0a5-82ee7a745b97',
+    name: 'Jane Doe',
+    email: 'jane@doe.com'
+  }
+});
+
+export default store;
